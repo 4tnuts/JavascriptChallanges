a6c27ebd8abcc343bff9cc4d7848e3a0e34689a3
ramanavami <48641207+4tnuts@users.noreply.github.com>
2019-12-15 17:53:12 +0800

    Chaged to es6 syntax - Sentence Manipulation Challenge

--- sentenceManipulation.js
+++ sentenceManipulation.js
@@ -1,24 +1,16 @@
-function sentenceManipulation(sentence) {
-    
-    let kataTerpisah = sentence.split(' ', sentence.length);
-    
-    let hasil = "";
-    
-    for (let i = 0, validasi = /\b[aiueo]/i; i < kataTerpisah.length; i++) {
-        
-        if (validasi.test(kataTerpisah[i][0]) == true) {
-        
-            hasil += kataTerpisah[i] + " ";
-        
+const sentenceManipulation = sentence => {
+    let splittedSentence = sentence.split(' ', sentence.length);
+    let result = "";
+
+    for (let i = 0, validasi = /\b[aiueo]/i; i < splittedSentence.length; i++) {
+        if (validasi.test(splittedSentence[i][0]) == true){ 
+            result += splittedSentence[i] + " ";
         } else {
-        
-            hasil += kataTerpisah[i].substring(1) + kataTerpisah[i][0] + "nyo ";
-        
+            result += splittedSentence[i].substring(1) + splittedSentence[i][0] + "nyo ";
         }
-    
     }
-    
-    console.log(hasil);
+
+    console.log(result);
 }
 
 sentenceManipulation("ibu pergi ke pasar bersama aku");
