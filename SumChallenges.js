836840494394af7856eefa97f11c5a830c1a24a2
ramanavami <48641207+4tnuts@users.noreply.github.com>
2019-12-15 17:26:59 +0800

    Changed variable type from var to let

--- SumChallenges.js
+++ SumChallenges.js
@@ -1,17 +1,16 @@
 function sum(){
-    var angka = Array.from(arguments);
     var hasil = 0;
-    for(var i = 0; i < angka.length; i++){
-        hasil+=angka[i];
+    for(var i = 0; i < arguments.length; i++){
+        hasil+=arguments[i];
     }
 console.log(hasil);
 }
-
 
 sum(1,2,7);
 sum(1,4);
 sum(11);
 sum(10,3,6,7,9);
+
 
 
 
