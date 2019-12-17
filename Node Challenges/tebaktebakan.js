38db2dd7630f84616734db6afaca7427a6328d47
4tnuts <ramanavami32@gmail.com>
2019-12-17 16:13:04 +0800

    Challenge 12 Completed

--- Node Challenges/tebaktebakan.js
+++ Node Challenges/tebaktebakan.js
@@ -3,6 +3,7 @@ const readline = require('readline');
 let inputArguments = process.argv.slice(1);
 let number = 0;
 let salah = 0;
+let skipped = 0;
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -14,31 +15,50 @@ if (inputArguments[1] === undefined && inputArguments !== 'data.json') {
     console.log(`Toloong sertakan nama file yang akan digunakan sebagai soal.`);
     console.log(`Misal 'node solution.js data.json'`);
     rl.close();
-}else{
+} else {
 
-fs.readFile(inputArguments[1], (err, data) => {
-    if (err) throw err;
-    const jsonData = JSON.parse(data);
-    console.log(`Selamat datang di permainan tebak - tebakan. kamu akan di berikan file ${inputArguments[1]}.\n
+    fs.readFile(inputArguments[1], (err, data) => {
+        if (err) throw err;
+        const jsonData = JSON.parse(data);
+        console.log(`Selamat datang di permaijsonDatanan tebak - tebakan. kamu akan di berikan file ${inputArguments[1]}.\n
 untuk bermain , jawablah dengan jawaban yang sesuai.\nGunakanlah 'skip' untuk menangguhkan pertanyaannya, dan di
 akhir pertanyaan akan di tanyakan lagi`);
-    console.log(`Pertanyaan : ${jsonData[number].definition}`);
-    rl.prompt();
-    rl.on('line', line => {
-        if (line === 'skip') {
-            number++;
-            console.log(`Pertanyaan : ${jsonData[number].definition}`)
-        }else if(line === jsonData[number].term){
-            console.log("selamat anda benar");
-            number++;
-            console.log(`Pertanyaan : ${jsonData[number].definition}`);
-        }else{
-            salah++;
-            console.log(`Anda salah ke ${salah} kalinya`)
-        }
-        rl.prompt()
-    }).on('close', () => {
-        process.exit(0);
+
+        console.log(`Pertanyaan : ${jsonData[0].definition}`);
+        rl.prompt();
+
+        rl.on('line', line => {
+            if (line === 'skip') {
+                jsonData.push(jsonData[skipped]);
+                jsonData.splice(skipped, 1);
+                salah = 0;
+                if (skipped == jsonData.length) {
+                    skipped = 0;
+                }
+                console.log(`Pertanyaan : ${jsonData[skipped].definition}`);
+            } else {
+                console.log(skipped);
+                if (line === jsonData[skipped].term) {
+                    console.log('Benar');
+                    number++;
+                    jsonData.splice(skipped, 1);
+                    skipped = 0;
+                    if (number !== 3) {
+                        console.log(`Pertanyaan : ${jsonData[skipped].definition}`);
+                    } else {
+                        console.log('Selamat anda menang');
+                        rl.close();
+                    }
+                } else {
+                    salah++;
+                    console.log(`salah ke ${salah}`);
+                    console.log('Pertanyaan :' + jsonData[skipped].definition);
+                }
+            }
+
+            rl.prompt()
+        }).on('close', () => {
+            process.exit(0);
+        });
     });
-});
 }
