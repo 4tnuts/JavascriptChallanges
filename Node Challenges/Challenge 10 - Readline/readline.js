ee868a296fe5b53f7305fb5925dc381d9bd175e6
4tnuts <ramanavami32@gmail.com>
2020-01-01 00:23:48 +0800

    removed a bug

--- Node Challenges/Challenge 10 - Readline/readline.js
+++ Node Challenges/Challenge 10 - Readline/readline.js
@@ -1,29 +1,31 @@
 const readline = require('readline');
 const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
-    prompt : 'Tulis kalimat mu disini >'
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tulis kalimat mu disini >'
 });
 
 const sentenceManipulation = sentence => {
-    let splittedSentence = sentence.split(' ', sentence.length);
+    let words = sentence.split(' ', sentence.length);
     let result = "";
 
-    for (let i = 0, validasi = /\b[aiueo]/i; i < splittedSentence.length; i++) {
-        if (validasi.test(splittedSentence[i][0]) == true){ 
-            result += splittedSentence[i] + " ";
+    for (let i = 0, validasi = /\b[aiueo]/i; i < words.length; i++) {
+        if (validasi.test(words[i][0]) == true){ 
+            result += words[i] + " ";
         } else {
-            result += splittedSentence[i].substring(1) + splittedSentence[i][0] + "nyo ";
+            result += words[i].concat(words[i][0], 'nyo ');
         }
     }
     console.log(result);
+    rl.prompt();
 }
 
+
 rl.prompt();
-rl.on('line', (line) =>{
-    sentenceManipulation(line);
-})
-.on('close', ()=>{
-    console.log('\nKeluar');
-    process.exit(0);
-})
+rl.on('line', (line) => {
+        sentenceManipulation(line);
+    })
+    .on('close', () => {
+        console.log('\nKeluar');
+        process.exit(0);
+    })
