2eeba1406f27ae61558fc1cbecec098f52a295c4
4tnuts <ramanavami32@gmail.com>
2020-01-24 03:39:17 +0700

    filtering statements done need some fixes

--- Node Challenges/Challenge 21 - PostgreSQL-BREAD/routes/bread.js
+++ Node Challenges/Challenge 21 - PostgreSQL-BREAD/routes/bread.js
@@ -8,19 +8,61 @@ router.use(bodyParser.json())
 
 module.exports = (pool) => {
   router.get('/', (req, res, next) => {
-    let read = 'SELECT * FROM bread';
-    pool.query(read, (err, data) => {
+    let resultQuery = 'SELECT count(*) FROM bread';
+    const limit = 3;
+    const currentPage = req.query.page || 1;
+    const offset = (currentPage - 1) * limit;
+    let queries = [];
+    if(req.query.idCheck && req.query.id){
+      queries.push(`id = ${req.query.id}`);
+    }
+    if(req.query.stringCheck && req.query.string){
+      queries.push(`teks LIKE '%${req.query.string}%'`);
+    }
+    if(req.query.integerCheck && req.query.integer){
+      queries.push(`nomor = ${req.query.integer}`);
+    }
+    if(req.query.floatCheck && req.query.float){
+      queries.push(`pecahan = ${req.query.float}`);
+    }
+    if(req.query.dateCheck && req.query.dateStart && req.query.dateEnd){
+      queries.push(`tanggal BETWEEN ${req.query.dateStart} AND ${req.query.dateEnd}`);
+    }
+    if(req.query.booleanCheck && req.query.boolean){
+      queries.push(`kondisi = ${req.query.boolean}`);
+    }
+    if(queries.length > 0){
+      resultQuery += ` WHERE ${queries.join(' AND ')}`;
+    }
+
+    pool.query(resultQuery, (err, data) => {
       if (err) return console.error(err);
-      let finalData = data.rows.map(item => {
-        item.tanggal = moment(item.tanggal).format('YYYY-MMM-DD');
-        return item;
+      const totalPage = Math.ceil(data.rows / limit);
+      const url = req.url == '/' ? '?page=1' : req.url;
+      resultQuery = `SELECT * FROM bread`;
+      if(queries.length > 0){
+        resultQuery += ` WHERE ${queries.join(' AND ')}`;
+      }
+      resultQuery+=` LIMIT ${limit} OFFSET ${offset}`;
+      pool.query(resultQuery, (err, data)=>{
+        res.status(200).json({
+          data: data.rows,
+          url,
+          totalPage,
+          currentPage : parseInt(currentPage),
+          query : req.query
+        });
       })
-      res.status(200).json(finalData)
     });
+    // let finalData = data.rows.map(item => {
+    //   item.tanggal = moment(item.tanggal).format('YYYY-MMM-DD');
+    //   return item;
+    // })
   });
 
   router.post('/', (req, res, next) => {
     let create = 'INSERT INTO bread (teks, nomor, pecahan, tanggal, kondisi) VALUES($1,$2,$3,$4,$5)';
+    console.log(req.body);
     let body = [req.body.teks, req.body.nomor, req.body.pecahan, req.body.tanggal, req.body.kondisi];
     pool.query(create, body, (err, data) => {
       if (err) return console.error(err);
@@ -54,7 +96,7 @@ module.exports = (pool) => {
     pool.query(update, body, (err, data) => {
       if (err) return console.error(err);
       res.status(201).json(data.rows = {
-        id: req.params.id,
+        id      : req.params.id,
         teks    : body[0],
         nomor   : body[1],
         pecahan : body[2],
