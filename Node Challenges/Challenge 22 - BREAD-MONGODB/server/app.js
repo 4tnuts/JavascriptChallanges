a226dbc793c648f7b4fa002c3787875bcac761a0
4tnuts <ramanavami32@gmail.com>
2020-01-28 17:55:50 +0700

    API Completed - need some checks

--- Node Challenges/Challenge 22 - BREAD-MONGODB/server/app.js
+++ Node Challenges/Challenge 22 - BREAD-MONGODB/server/app.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+
+const indexRouter = require('./routes/index');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'mongobread';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  
+  const db = client.db(dbName);
+  const usersRouter = require('./routes/users')(db);
+  app.use('/users', usersRouter);
+});
 
 module.exports = app;
--- Node Challenges/Challenge 22 - BREAD-MONGODB/server/routes/users.js
+++ Node Challenges/Challenge 22 - BREAD-MONGODB/server/routes/users.js
@@ -1,9 +1,58 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const ObjectId = require('mongodb').ObjectId
+module.exports = (db) => {
+    const collection = db.collection('bread')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+    router.get('/', (req, res, next)=>{
+        collection.find().toArray((err, result) => {
+            if(err) return console.error(err);
+            res.status(302).json(result);
+        })
+    })
 
-module.exports = router;
+    router.get('/:id', (req, res, next)=>{
+        collection.find({_id : ObjectId(req.params.id)}).toArray((err, result) => {
+            if(err) return console.error(err);
+            res.status(302).json(result);
+        })
+    })
+
+    router.post('/', (req, res, next)=>{
+        const data = {
+            string  : req.body.string,
+            number  : parseInt(req.body.number),
+            float   : parseFloat(req.body.float),
+            date    : new Date(req.body.date),
+            boolean : req.body.boolean
+        }
+        collection.save(data, (err, result) => {
+            if(err) return console.error(err);
+            res.status(201).json(result);
+        })
+    })
+
+    router.put('/', (req,res,next) => {
+        collection.findOneAndUpdate({_id : ObjectId(req.body.id)},{
+            $set : {
+                string  : req.body.string,
+                number  : parseInt(req.body.number),
+                float   : parseFloat(req.body.float),
+                date    : new Date(req.body.date),
+                boolean : req.body.boolean
+            }
+        },
+        (err,result)=>{
+            if(err) return console.error(err);
+            res.status(200).json(result);
+        })
+    })
+
+    router.delete('/:id', (req,res,next) => {
+        collection.findOneAndDelete({_id : ObjectId(req.params.id)}, (err,result)=>{
+            if(err) return console.error(err);
+            res.status(200).json(result);
+        })
+    })
+    return router;
+}
